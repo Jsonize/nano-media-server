1f767a9a270bc0cc044ca0d94582a49ad8de1aa8
Rashad Aliyev <aliyev.resad@gmail.com>
2018-09-24 15:10:09 +0400

    Added only audio transcoding path

--- src/server_service.js
+++ src/server_service.js
@@ -5,92 +5,111 @@ var Ffmpeg = require("js-ffmpeg");
 
 module.exports = {
 
-	init: function (Config) {
-		var upload = Multer({ storage: Multer.memoryStorage() });
-        var ffmpegOptions = Config.ffmpegOptions;
+  init: function (Config) {
+    var upload = Multer({ storage: Multer.memoryStorage() });
+    var ffmpegOptions = Config.ffmpegOptions;
 
-		var express = Express();
+    var express = Express();
 
-		if (Config.staticServe)
-			express.use("/static", Express["static"](Config.staticServe));
+    if (Config.staticServe)
+      express.use("/static", Express["static"](Config.staticServe));
 
-		var fullFile = function (filename) {
-			return Config.mediaDirectory + "/" + filename;
-		};
+    var fullFile = function (filename) {
+      return Config.mediaDirectory + "/" + filename;
+    };
 
-		express.use(function(request, response, next) {
-			response.header("Access-Control-Allow-Origin", "*");
-			response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-			next();
-		});
+    express.use(function(request, response, next) {
+      response.header("Access-Control-Allow-Origin", "*");
+      response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 
-		express.get('/files/:filename', function (request, response) {
-			console.log("Streaming " + request.params.filename);
-			response.sendFile(fullFile(request.params.filename));
-		});
+    express.get('/files/:filename', function (request, response) {
+      console.log("Streaming " + request.params.filename);
+      response.sendFile(fullFile(request.params.filename));
+    });
 
-		express.post("/files/:filename", upload.single('file'), function (request, response) {
-			console.log("Storing " + request.params.filename);
-			FS.writeFileSync(fullFile(request.params.filename), request.file.buffer);
-			response.status(200).send({});
-		});
+    express.post("/files/:filename", upload.single('file'), function (request, response) {
+      console.log("Storing " + request.params.filename);
+      FS.writeFileSync(fullFile(request.params.filename), request.file.buffer);
+      response.status(200).send({});
+    });
 
-		express.post("/files/:source/transcode/:target", function (request, response) {
-			var target = fullFile(request.params.target);
-			var video_source = fullFile(request.params.source);
-			var audio_source = request.query.audio ? fullFile(request.query.audio) : null;
-			var sources = [video_source];
-			if (audio_source)
-				sources.push(audio_souce);
-			console.log("Transcoding " + request.params.source + (request.query.audio ? " + " + request.query.audio : "") + " -> " + request.params.target);
-			Ffmpeg.ffmpeg_simple(sources, {}, target, null, null, ffmpegOptions).callback(function (error, value) {
-				if (error)
-					response.status(500).send(error);
-				else
-					response.status(200).send(value);
-			});
-		});
+    express.post("/files/:source/transcode/:target", function (request, response) {
+      var target = fullFile(request.params.target);
+      var video_source = fullFile(request.params.source);
+      var audio_source = request.query.audio ? fullFile(request.query.audio) : null;
+      var sources = [video_source];
+      if (audio_source)
+        sources.push(audio_souce);
+      console.log("Transcoding " + request.params.source + (request.query.audio ? " + " + request.query.audio : "") + " -> " + request.params.target);
+      Ffmpeg.ffmpeg_simple(sources, {}, target, null, null, ffmpegOptions).callback(function (error, value) {
+        if (error)
+          response.status(500).send(error);
+        else
+          response.status(200).send(value);
+      });
+    });
 
-        express.post("/text-track/:filename/lang/:lang/label/:label", upload.single('file'), function (request, response) {
-        	var _params = request.params;
-            console.log("Storing text tracks" + _params.filename);
-            // request.file has keys: fieldname, fieldname: 'file', originalname: 'bunny-en.vtt',
-            // encoding: '7bit', mimetype: 'application/octet-stream', buffer: <Buffer ....>, size: 472 }
-			// request.params has only one key filename: 'video-timestamp.vtt'
-            FS.writeFileSync(fullFile(_params.filename), request.file.buffer);
-            response.status(200).send({'lang': _params.lang, 'label': _params.label, 'filename': _params.filename});
-        });
+    // Post recorder/uploaded new audio file to the server temporary folder
+    express.post("/audio/:source/transcode/:target", function (request, response) {
+      var target = fullFile(request.params.target);
+      var audio_source = fullFile(request.params.source);
+      if (!audio_source) response.status(500).send("No audio source provided");
 
-        express.get("/text-track/:filename", function(request, response) {
-            console.log("Streaming text tracks" + request.params.filename);
-			response.sendFile(fullFile(request.params.filename));
-        });
+      var sources = [audio_source];
 
-        express.get("/text-track/:filename/lang/:lang/label/:label", function(request, response) {
-            console.log("Streaming detailed text tracks lang:" + request.params.lang + ', label: ' + request.params.label + ', filename: ' + request.params.filename);
-            response.status(200).sendFile(fullFile(request.params.filename));
-            // response.status(200).send({'filename': request.params.filename,'lang': request.params.lang, 'label': request.params.label});
-        });
+      console.log('->>', sources);
 
-		return express;
-	},
+      console.log("Transcoding " + request.params.source + " -> " + request.params.target);
+      Ffmpeg.ffmpeg_simple(sources, {output_type: "audio"}, target, null, null, ffmpegOptions).callback(function (error, value) {
+        if (error)
+          response.status(500).send(error);
+        else
+          response.status(200).send(value);
+      });
+    });
 
-	run: function (server, port, sslKey, sslCert) {
-		port = port || 5000;
-		if (sslKey && sslCert) {
-			require("https").createServer({
-				key: FS.readFileSync(sslKey),
-				cert: FS.readFileSync(sslCert),
-				requestCert: false,
-				rejectUnauthorized: false
-			}, server).listen(port, function () {
-				console.log("Listening on", port);
-			});
-		} else {
-			server.listen(port, function () {
-				console.log("Listening on", port);
-			});
-		}
-	}
+    express.post("/text-track/:filename/lang/:lang/label/:label", upload.single('file'), function (request, response) {
+      var _params = request.params;
+      console.log("Storing text tracks" + _params.filename);
+      // request.file has keys: fieldname, fieldname: 'file', originalname: 'bunny-en.vtt',
+      // encoding: '7bit', mimetype: 'application/octet-stream', buffer: <Buffer ....>, size: 472 }
+      // request.params has only one key filename: 'video-timestamp.vtt'
+      FS.writeFileSync(fullFile(_params.filename), request.file.buffer);
+      response.status(200).send({'lang': _params.lang, 'label': _params.label, 'filename': _params.filename});
+    });
+
+    express.get("/text-track/:filename", function(request, response) {
+      console.log("Streaming text tracks" + request.params.filename);
+      response.sendFile(fullFile(request.params.filename));
+    });
+
+    express.get("/text-track/:filename/lang/:lang/label/:label", function(request, response) {
+      console.log("Streaming detailed text tracks lang:" + request.params.lang + ', label: ' + request.params.label + ', filename: ' + request.params.filename);
+      response.status(200).sendFile(fullFile(request.params.filename));
+      // response.status(200).send({'filename': request.params.filename,'lang': request.params.lang, 'label': request.params.label});
+    });
+
+    return express;
+  },
+
+  run: function (server, port, sslKey, sslCert) {
+    port = port || 5000;
+    if (sslKey && sslCert) {
+      require("https").createServer({
+        key: FS.readFileSync(sslKey),
+        cert: FS.readFileSync(sslCert),
+        requestCert: false,
+        rejectUnauthorized: false
+      }, server).listen(port, function () {
+        console.log("Listening on", port);
+      });
+    } else {
+      server.listen(port, function () {
+        console.log("Listening on", port);
+      });
+    }
+  }
 
 };
